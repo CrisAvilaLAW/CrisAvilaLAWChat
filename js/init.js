570766dbb04574bd482f575593aa692a083b5acc
CrisAvilaLAW <cristophervalier.cv@gmail.com>
2021-05-25 20:02:53 -0500

    cambioo

---
+++ js/init.js
@@ -0,0 +1,13 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyB-dEvRvsH52CttfwMWYQq72mytqVcr1gk",
+    authDomain: "fbwebchat41.firebaseapp.com",
+    projectId: "fbwebchat41",
+    storageBucket: "fbwebchat41.appspot.com",
+    messagingSenderId: "221526640458",
+    appId: "1:221526640458:web:26522011a84c778e3023b3",
+    measurementId: "G-681RNST4ZK"
+    });
+  
